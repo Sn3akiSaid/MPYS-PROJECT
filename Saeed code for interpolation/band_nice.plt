b0f2ea330f3d2f49d524296c5113a15910dfa44d
Sn3akiSaid <taragoshevaleksandar@gmail.com>
2024-10-31 12:27:43 +0000

    Band gap simple plot against alpha, some modifications to the band plotting script (band_nice.plt) to be executed instead of band.plt when the fortran code is rewritten. Charlie's mesh module. Some small modifications to  the interpolate.f90 code that are commented.

--- Saeed code for interpolation/band_nice.plt
+++ Saeed code for interpolation/band_nice.plt
@@ -10,12 +10,13 @@ set size ratio 0 1.0,1.0
 set ylabel "E-E_{VBM} (eV)"
 set yrange [ -0.5 : 0.5 ]
 unset key
-set ytics 1.0 scale 1 nomirror out
+set ytics 0.1 scale 1 nomirror out
 set mytics 2
 set terminal qt persist
 set parametric
 set trange [-10:10]
-plot for [i=1:20]sprintf("band_partition_%d.dat", i) u 1:2 with l lt 1 lw 0.5,\
 
-    0.000000,t with l lt 2  lc -1,\
-t,0 with l lt 2  lc -1
+#iterates over all the partition files to plot on the same graph
+plot for [i=1:50] sprintf("band_partition_%d.dat", i) u 1:2 with l lt 1 lw 0.5, \
+    0.0, t with l lt 2 lc -1, \
+    t, 0.0 with l lt 2 lc -1
--- Saeed code for interpolation/src/interpolate.f90
+++ Saeed code for interpolation/src/interpolate.f90
@@ -133,11 +133,13 @@
                                        dcmplx(cos(phase_topological),-sin(phase_topological))/float(ndeg_topological(j))
                   enddo
                enddo
-   
+            !print *, alpha
             enddo
 
             HK=alpha*HK_trivial+(1d0-alpha)*HK_topological
-   
+            !print *, HK
+            !HK_crit = minval(abs(HK))
+           ! print *, HK_crit
             call zheev('V','U',nb,Hk,nb,ene(:,k),work,lwork,rwork,info)
             
          enddo
@@ -159,7 +161,7 @@
          enddo
          close(100)
 !------- export gap
-         write(777,'(i5,f10.8,f8.5)')ipart,alpha,gap(ipart)
+         write(777,'(f10.8,f8.5)') alpha,gap(ipart) !this had ipart in front of alpha
       enddo
 !------- call plt script of gnuploting
       call write_plt(nkpath,xkl,klabel)
@@ -207,7 +209,7 @@
            'set terminal qt persist',&
            'set parametric',&
            'set trange [-10:10]',&
-           'plot for [i=1:20]sprintf("band_partition_%d.dat", i) u 1:2 with l lt 1 lw ,\'
+           'plot for [i=1:20]sprintf("band_partition_%d.dat", i) u 1:2 with l lt 1 lw 0.5,\'
      do i=2,nkp-1
        write(99,'(f12.6,a)') xkl(i),',t with l lt 2  lc -1,\'
      enddo

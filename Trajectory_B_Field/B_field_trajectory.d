eb4a07c3c884427bc52f56ee23b47d2df4ce5254
Charlie Ellis <charlie.d.ellis@hotmail.co.uk>
2025-02-11 16:35:33 +0000

    trajectory_b_field

---
+++ Trajectory_B_Field/B_field_trajectory.d
@@ -0,0 +1,2 @@
+obj/B_field_trajectory.o B_field_trajectory.o: src/B_field_trajectory.f90 \
+ /usr/include/finclude/x86_64-linux-gnu/math-vector-fortran.h
---
+++ Trajectory_B_Field/trajectory_xyz.d
@@ -0,0 +1,2 @@
+obj/trajectory_xyz.o trajectory_xyz.o: src/trajectory_xyz.f90 \
+ /usr/include/finclude/x86_64-linux-gnu/math-vector-fortran.h

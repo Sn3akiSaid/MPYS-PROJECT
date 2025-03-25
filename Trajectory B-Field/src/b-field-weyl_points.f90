ea7765f60880f7916f7d1c1929a6c50dff8a114b
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-03-25 14:55:55 +0000

    Fully working newly implemented mesh generation routine. Still issues with 4x4 hamil.

--- Trajectory B-Field/src/b-field-weyl_points.f90
+++ Trajectory B-Field/src/b-field-weyl_points.f90
@@ -11,15 +11,15 @@ program generate_fermi
 !--------Presets to be changed by User
     character(len=80):: prefix="BiTeI"
     !Adjust these parameters to obtain better resolution around alphacrit and see points closer to an effectively closed gap
-    integer,parameter::np=30,npartitions=10,dim=3
+    integer,parameter::np=50,npartitions=5,dim=3
          ! Flags
-    logical :: useOptimized = .false.  ! Set to false for 4x4 case
+    logical :: useOptimized = .true.  ! Set to false for 4x4 case
     
     real*8,parameter::B_x = 0d0, B_y = 0.05d0, B_z = 0d0,& !Run again at B_y=0.05-0.06 to see the gap close
-                      alpha_min = 0.57d0, alpha_max = 0.635d0
+                    !   alpha_min = 0.592d0, alpha_max = 0.635d0
                     !   alpha_min = 0d0, alpha_max = 1d0
 
-                    !   alpha_min = 0.76d0, alpha_max = 0.79d0
+                      alpha_min = 0.793d0, alpha_max = 0.815d0
 
 !---------MPI variables
     integer :: ierr, nprocs, rank, local_start, local_end, local_count
@@ -27,7 +27,7 @@ program generate_fermi
 
 !---------Variable allocation
     character(len=80) :: hamil_file_trivial, hamil_file_topological, nnkp, line, partnumber
-    character(len=200) :: hamil_dir = '/home/aleks/MPYS-PROJECT/Hamiltonians 4x4/'
+    character(len=200) :: hamil_dir = '/home/aleks/MPYS-PROJECT/Hamiltonians 18x18/'
 
     integer ik, ipart, ib, is,&
             i,j,k,&
@@ -75,9 +75,9 @@ program generate_fermi
                              Hamr_trivial(:,:,:), Hamr_topological(:,:,:),&
                              work(:)
 
-    real*8, parameter :: kbox_x=0.065d0,&!x_min = -0.06d0, x_max = 0.06d0,&
-                         kbox_y=0.065d0,&!y_min = -0.06d0, y_max = 0.06d0,&
-                         kbox_z=0.03d0!z_min = -0.03d0, z_max = 0.03d0
+    real*8, parameter :: kbox_x=0.12d0,&!x_min = -0.06d0, x_max = 0.06d0,&
+                         kbox_y=0.12d0,&!y_min = -0.06d0, y_max = 0.06d0,&
+                         kbox_z=0.04d0!z_min = -0.03d0, z_max = 0.03d0
 
     integer, dimension(:), allocatable:: indices
 
@@ -156,7 +156,7 @@ allocate(ndeg_trivial(nr_trivial),ndeg_topological(nr_topological))
                 Hamr_trivial, Hamr_topological, rvec_trivial, rvec_topological, ndeg_trivial, ndeg_topological, avec)
             endif
         endif
-allocate(enep(nb, (np+1)**dim), ene(nb, (np+1)**dim))
+allocate(enep(nb, (2*np+1)**dim), ene(nb, (2*np+1)**dim))
 ! Broadcast the read data to all processes
     call MPI_BCAST(ndeg_trivial, nr_trivial, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
     call MPI_BCAST(ndeg_topological, nr_topological, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
@@ -172,14 +172,13 @@ allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
     allocate(mesh(3, (2*np+1)**dim))
 !------ open gap file
     open(777,file='gap.dat',status='replace', position='append', action='write')
-    open(100,file='trajectory_4_0.001.dat',status='replace', position='append', action='write')
-    open(110,file='trajectory_p_4_0.001.dat',status='replace', position='append', action='write')
+    open(100,file='trajectory_18_0.001.dat',status='old', position='append', action='write', iostat=ierr)
+    if (ierr /= 0) then
+        open(100, file='trajectory_18_0.001.dat', status='new', action='write')
+    end if
+    ! open(110,file='trajectory_p_18_0.001.dat',status='replace', position='append', action='write')
 
 !-------Generate Mesh
-
-    ! dx = (x_max - x_min)/np
-    ! dy = (y_max - y_min)/np
-    ! dz = (z_max - z_min)/np
     delkx = kbox_x/(2*np+1)
     delky = kbox_y/(2*np+1)
     delkz = kbox_z/(2*np+1)
@@ -190,24 +189,28 @@ allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
             do k = -np, np
                 j = j+1  ! Direct index calculation
                 ! Calculate coordinates directly
-                mesh(1, j) = i*delkx!(x_min + i * dx )!* bvec(1,1)  ! = i*delkx
-                mesh(2, j) = n*delky!(y_min + n * dy )!* (bvec(1,2)+bvec(2,2)) ! = n*delky
-                mesh(3, j) = k*delkz+0.5d0*bvec(3,3)!(z_min + k * dz ) + 0.5*bvec(3,3) ! = k*delkz
+                mesh(1, j) = i*delkx
+                mesh(2, j) = n*delky
+                mesh(3, j) = k*delkz
             end do
         end do
     end do
-    
+    mesh(3,:)=mesh(3,:)+0.5d0*bvec(3,3)
+    if (rank == 0) then
+        write(*,*) "Total mesh points generated:", j
+        write(*,*) "Expected mesh points:", (2*np+1)**3
+    endif
 !------ Magnetic Field
-    allocate(Hm(2,2),Hmag(nb,nb))
-    Hm = B_x*sigx + B_y*sigy + B_z*sigz
+    ! allocate(Hm(2,2),Hmag(nb,nb))
+    ! Hm = B_x*sigx + B_y*sigy + B_z*sigz
 !------ Turn Hm into an 18x18 to match Hk      
-    Hmag = (0d0, 0d0)
-    do i=1, nb/2
-          Hmag(i,i)=Hm(1,1)
-          Hmag(i,i+nb/2)=Hm(1,2)
-          Hmag(i+nb/2,i)=Hm(2,1)
-          Hmag(i+nb/2,i+nb/2)=Hm(2,2)
-    end do
+    ! Hmag = (0d0, 0d0)
+    ! do i=1, nb/2
+    !       Hmag(i,i)=Hm(1,1)
+    !       Hmag(i,i+nb/2)=Hm(1,2)
+    !       Hmag(i+nb/2,i)=Hm(2,1)
+    !       Hmag(i+nb/2,i+nb/2)=Hm(2,2)
+    ! end do
 !------ Fourrier transform H(R) to H(k)
     ! allocate(phases(nr, (np+1)**dim))
 
@@ -236,23 +239,25 @@ allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
     !    ene=0d0
 
 ! !----- FOURIER TRANSFORM 
-       call fourier_transform_general(2*np+1, dim, nr_trivial, nr_topological, nb, ndeg_trivial, ndeg_topological, mesh,&
-                                      rvec_trivial, rvec_topological, Hamr_trivial, Hamr_topological,&
-                                      Hmag, alpha, enep, ene, work, lwork, rwork, rank, ierr)
+    !    call fourier_transform_general(2*np, dim, nr_trivial, nr_topological, nb, ndeg_trivial, ndeg_topological, mesh,&
+    !                                   rvec_trivial, rvec_topological, Hamr_trivial, Hamr_topological,&
+    !                                   Hmag, alpha, enep, ene, work, lwork, rwork, rank, ierr)
 
-    !    call fourier_transform_optimized(np, dim, nr_trivial, nb, ndeg_trivial, mesh, rvec_trivial, &
-    !                                     Hamr_trivial, Hamr_topological, Hmag, alpha, &
-    !                                     enep, ene, work, lwork, rwork, rank, ierr)
+       call fourier_transform_optimized(2*np, dim, nr_trivial, nb, ndeg_trivial, mesh, rvec_trivial, &
+                                        Hamr_trivial, Hamr_topological, Hmag, alpha, &
+                                        enep, ene, work, lwork, rwork, rank, ierr)
 
 !----- END FOURIER TRANSFORM
 
 
 !------calcualte gap and Fermi level
     !    print *, minval(ene(3,:)),maxval(ene(2,:))
-       gap_min(ipart)=abs(minval(ene(3,:))-maxval(ene(2,:)))
+    !    gap_min(ipart)=abs(minval(ene(3,:))-maxval(ene(2,:)))
+    !    print *, "MINGAP: ", gap_min(ipart)
+    !    gapp_min(ipart)=abs(minval(enep(3,:))-maxval(enep(2,:)))
+       gap_min(ipart)=abs(minval(ene(13,:))-maxval(ene(12,:)))
        print *, "MINGAP: ", gap_min(ipart)
-       gapp_min(ipart)=abs(minval(enep(3,:))-maxval(enep(2,:)))
-
+       gapp_min(ipart)=abs(minval(enep(13,:))-maxval(enep(12,:)))
     !    ef(ipart)=(minval(ene(3,:))+maxval(ene(2,:)))/2d0
     !    efp(ipart)=(minval(enep(13,:))+maxval(enep(12,:)))/2d0
     !    gap_perturbed(ipart)=minval(enep(13,:))-maxval(enep(12,:))
@@ -271,19 +276,21 @@ allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
         !   do k=1,(np+1)**3
         !         write(100, '(5(x,f12.6))') mesh(1:3,k), ene(12:13,k)- ef(ipart)
         !   end do
-            do k=1,(np+1)**dim
-                bandgap=ene(3,k)-ene(2,k)
-                bandgapp=enep(3,k)-enep(2,k)
+            do k=1,(2*np+1)**dim
+                ! bandgap=ene(3,k)-ene(2,k)
+                ! bandgapp=enep(3,k)-enep(2,k)
+                bandgap=ene(13,k)-ene(12,k)
+                bandgapp=enep(13,k)-enep(12,k)
 
                 ! print*, bandgap
-                if (abs(bandgap-(gap_min(ipart))) .lt. 0.01d0) then
+                if (abs(bandgap-(gap_min(ipart))) .lt. 0.001d0) then
                     print*, abs(bandgap-(gap_min(ipart)))
                     write(100, '(5(x,f12.6))') mesh(1:3,k), alpha, abs(bandgap-(gap_min(ipart)))!, gap_min(ipart)
                 endif
-                if (abs(bandgapp-(gapp_min(ipart))) .lt. 0.01d0) then
-                    print*, abs(bandgapp-(gapp_min(ipart)))
-                    write(110, '(5(x,f12.6))') mesh(1:3,k), alpha, abs(bandgapp-(gapp_min(ipart)))!, gap_min(ipart)
-                endif
+                ! if (abs(bandgapp-(gapp_min(ipart))) .lt. 0.001d0) then
+                !     print*, abs(bandgapp-(gapp_min(ipart)))
+                !     write(110, '(5(x,f12.6))') mesh(1:3,k), alpha, abs(bandgapp-(gapp_min(ipart)))!, gap_min(ipart)
+                ! endif
             end do
 
             write(100,*)
